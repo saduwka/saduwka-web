e924d32286cc4bd9fa5205b5f10694ec5e65c97c
saduwka <nurzhan13071996@gmail.com>
2024-11-18 21:11:18 +0500

    Js1Homework

---
+++ js/homework/Sadu N. JS 1 homework/main.js
@@ -0,0 +1,28 @@
+let firstName = "Sadu";
+let age = "28";
+let city = "Astana";
+
+let nameType = typeof(firstName);
+let ageType = typeof(age);
+let cityType = typeof(city);
+
+
+console.log(firstName, age, city);
+console.log(nameType, ageType,cityType);
+
+let userAge = Number(prompt("Введите ваш возраст"));
+
+console.log("Ваш возраст =", userAge, "года/лет");
+
+let a = Number(prompt("Введите число a"));
+let b = Number(prompt("Введите число b"));
+
+let sum = (a + b);
+let diff = (a - b);
+let multiply = (a * b);
+let divide = (a / b);
+
+console.log("Сумма =", sum);
+console.log("Разница =", diff);
+console.log("Умножение =", multiply);
+console.log("Деление =", divide);
