0043b5c3a9c69b3d773d1fd0c9bca8d98460dd22
saduwka <nurzhan13071996@gmail.com>
2024-11-20 20:30:25 +0500

    JSlesson3

---
+++ js/3 lesson/main.js
@@ -0,0 +1,108 @@
+// let num = 10;
+
+// while (num >= 0) {
+//     console.log("number =", num);
+//     // num = num - 1;
+//     num -= 1;
+// }
+
+// let num1 = 0;
+
+// while (num1 <= 100) {
+//     if (num1 % 2 == 0){
+
+//     console.log("number2 =", num1);
+//     }
+//     num1 += 1;
+// }
+
+// // increment - увеличение на 1 - a++
+// // decrement - уменьшение на 1 - a--
+
+// let a = 1; 
+
+// while (a <= 10) {
+//     console.log(a);
+    
+//     a++;
+// }
+
+// let a = 0;
+
+// let counter = 0;
+
+// while (a <= 100) {
+//     if (a % 3 == 0 || a % 7 == 0) {
+//         counter++;
+//     }
+//     a++;
+    
+// }
+
+// console.log(counter);
+
+// let num = +prompt();
+
+// let counter = 0;
+
+// while (num > 1) {
+//     if (num <= 0) {
+//         console.log("Error input");
+//         break;
+//     }
+//     if (num % 2 == 0) {
+//         num = num / 2;        
+//     } else {
+//         num = (num * 3) + 1;
+//     }
+//     counter ++;
+// }
+
+// console.log(counter);
+
+// let a = 1;
+
+// while (a < 10) {
+//     a++;
+
+
+//     if (a % 2 == 0) {
+//         continue;
+//     }
+
+//     console.log(a);
+// }
+
+// let num = 1;
+// let sum = 0;
+
+
+// while (num <= 100) {
+//     console.log(sum, '+', num);
+//     sum = sum + num;
+//     num++;
+// }
+
+// console.log(sum);
+
+let num = 2;
+let counter = 0;
+
+while (num <= 1500) {
+    num = num ** 2;
+    counter++;
+}
+
+console.log(counter);
+
+
+
+
+
+
+
+
+
+
+
+    
